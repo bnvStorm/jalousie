31d3594d7dc3d607e1bb866520ff1686ab9406a0
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2020-12-02 11:44:32 +0600

    prefinal

---
+++ send.php
@@ -0,0 +1,27 @@
+
+<?php 
+var_dump($_POST);
+if(isset($_POST['submit'])){
+    $to = "bnvstorm@gmail.com"; // Здесь нужно написать e-mail, куда будут приходить письма
+    $from = "mail@vostok-zhalyuzi.kz"; // this is the sender's Email address  $_POST['message']@vostok-zhalyuzikz$$N
+    // $name = $_POST['name']; 
+    $subject = "Форма отправки сообщений с сайта";
+    $subject2 = "Copy of your form submission";
+    $message = "Клиент c номером " . $_POST['email'] . " оставил заявку на звонок!" . "\n\n";
+    $message2 = "Here is a copy of your message " . $email . "\n\n";
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    $test = mail($to,$subject,$message,$headers);
+    var_dump($test);
+   // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
+    echo "Сообщение отправлено. Спасибо Вам " . $email . ", мы скоро свяжемся с Вами.";
+	echo "<br /><br /><a href='http://jalousie/'>Вернуться на сайт.</a>";
+
+}
+
+?>
+<!--Переадресация на главную страницу сайта, через 3 секунды-->
+<script language="JavaScript" type="text/javascript">
+function changeurl(){eval(self.location="http://jalousie/");}
+window.setTimeout("changeurl();",2000);
+</script>
